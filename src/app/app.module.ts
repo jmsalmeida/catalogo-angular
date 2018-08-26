6e86fe63a17e68203b9f1af6333c22b5d71d3320
James Almeida <james.almeida.ti@gmail.com>
2018-08-26 18:03:40 -0300

    Adicionar component catalogList

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { AddRegisterFormComponent } from './add-register-form/add-register-form.component';
+import { CatalogListComponent } from './catalog-list/catalog-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    AddRegisterFormComponent
+    AddRegisterFormComponent,
+    CatalogListComponent
   ],
   imports: [
     BrowserModule
